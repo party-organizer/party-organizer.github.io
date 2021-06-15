9fa3734a199c8b6a34e3870bc46968e10093476f
latedude2 <simasc10@gmail.com>
2021-06-15 09:36:12 +0200

    cleanup map

--- map.js
+++ map.js
@@ -39,37 +39,6 @@ function initMap() {
         draggable:true,
         title:"Drag me!"
     });
-
-    const buttons = [
-      ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
-      ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
-      ["Tilt Down", "tilt", 20, google.maps.ControlPosition.TOP_CENTER],
-      ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
-    ];
-    buttons.forEach(([text, mode, amount, position]) => {
-      const controlDiv = document.createElement("div");
-      const controlUI = document.createElement("button");
-      controlUI.classList.add("ui-button");
-      controlUI.innerText = `${text}`;
-      controlUI.addEventListener("click", () => {
-        adjustMap(mode, amount);
-      });
-      controlDiv.appendChild(controlUI);
-      map.controls[position].push(controlDiv);
-    });
-  
-    const adjustMap = function (mode, amount) {
-      switch (mode) {
-        case "tilt":
-          map.setTilt(map.getTilt() + amount);
-          break;
-        case "rotate":
-          map.setHeading(map.getHeading() + amount);
-          break;
-        default:
-          break;
-      }
-    };
   }
 
   function setCoordinates(){
